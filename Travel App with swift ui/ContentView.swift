a2d8c7e6483a6ef883f595a47c269e96cc827816
Mahmoud Abdelhady <eng.mahmoud.sh.77@gmail.com>
2024-06-28 21:49:16 +0300

    Initial Commit

---
+++ Travel App with swift ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Travel App with swift ui
+//
+//  Created by Apple on 28/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Travel App with swift ui/Travel_App_with_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_App_with_swift_uiApp.swift
+//  Travel App with swift ui
+//
+//  Created by Apple on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Travel_App_with_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
